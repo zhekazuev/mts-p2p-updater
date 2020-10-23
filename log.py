4661ff70b8d06e56db4634b75b9e65e5557fa335
Eugene Zuev <zhekazuev@gmail.com>
2020-10-23 12:05:42 +0300

    Add logging for client.py

---
+++ log.py
@@ -0,0 +1,28 @@
+"""Custom logging."""
+from sys import stdout
+from loguru import logger as custom_logger
+
+
+def create_logger():
+    """Create custom logger."""
+    custom_logger.remove()
+    custom_logger.add(
+        stdout,
+        colorize=True,
+        level="INFO",
+        format="<light-cyan>{time:MM-DD-YYYY HH:mm:ss}</light-cyan> | "
+               "<light-green>{level}</light-green>: \
+               <light-blue>{message}</light-blue>")
+    custom_logger.add(
+        'logs/errors.log',
+        colorize=True,
+        level="ERROR",
+        rotation="200 MB",
+        catch=True,
+        format="<light-cyan>{time:MM-DD-YYYY HH:mm:ss}</light-cyan> | "
+               "<light-red>{level}</light-red>: "
+               "<light-blue>{message}</light-blue>")
+    return custom_logger
+
+
+logger = create_logger()
