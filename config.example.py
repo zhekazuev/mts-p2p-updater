f7ebcdc0fd769f41599d4b44f7492c82bc99d4bd
Eugene Zuev <zhekazuev@gmail.com>
2020-10-26 15:04:57 +0300

    Add config.example.py

--- config.example.py
+++ config.example.py
@@ -1,15 +1,11 @@
 import os
 
 
-script_user = os.environ["SCRIPTS_USER"]
-script_password = os.environ["SCRIPTS_PASS"]
-
-hosts = [('hostname1', '1.1.1.1'), ('hostname2', '1.1.1.2')]
-
-
 class StarOS:
-    script_user = 'user'
-    script_password = 'password'
+    # script_user = 'user'
+    # script_password = 'password'
+    script_user = os.environ["SCRIPTS_USER"]
+    script_password = os.environ["SCRIPTS_PASS"]
     # STAROS_SCRIPTS_TACACS_USER = os.environ['STAROS_SCRIPTS_TACACS_USER']
     # STAROS_SCRIPTS_TACACS_PASS = os.environ['STAROS_SCRIPTS_TACACS_PASS']
 
